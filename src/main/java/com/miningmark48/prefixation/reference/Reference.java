a4b8994d3537a393661b1df1d6a03d986e41ce83
MiningMark48 <miningmark48@gmail.com>
2017-11-19 23:27:21 -0500

    Update?

--- src/main/java/com/miningmark48/prefixation/reference/Reference.java
+++ src/main/java/com/miningmark48/prefixation/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "prefixation";
     public static final String MOD_NAME = "Prefixation";
-    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_VERSION = "0.0.2";
     public static final String VERSION = "1.12.2-" + MOD_VERSION;
     public static final String MOD_DEPENDENCIES = "required-after:mininglib@[1.1.0,);";
     public static final String CLIENT_PROXY_CLASS = "com.miningmark48.prefixation.proxy.ClientProxy";
